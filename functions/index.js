f59e31a668486a5647ba5391c7af630dc5377cbe
brahimir <brahimir.nick@gmail.com>
2020-12-20 03:50:45 -0500

    Updating some docs.

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,14 @@
+// Initialize Firebase Functions
 const functions = require('firebase-functions');
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
 
 const express = require("express");
 const bodyParser = require("body-parser");
+// TODO - Add cors.
 // const cors = require("cors");
 
 const app = express();
 
+// todo - Add origins to cors.
 // var corsOptions = {
 //   origin: "*"
 // };
@@ -45,7 +40,7 @@ db.mongoose
 // ! start:: ROUTES
 // Landing route
 app.get("/", (req, res) => {
-  res.json({ message: "LANDING -THE COMPENDIUM SERVER" });
+  res.send("LANDING -THE COMPENDIUM SERVER");
 });
 
 // Homebrew Weapons Routes
